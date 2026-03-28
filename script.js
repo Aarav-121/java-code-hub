d2a0625500f988a81692ff8ee0df8b4dcdb0c89e
Aarav-121 <aaravbnsheth@hotmail.com>
2026-03-28 20:19:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -105,5 +105,5 @@ function startConfetti(){
 
 // START MATRIX INTRO ON PAGE LOAD
 window.onload = () => {
-    startMatrixIntro(3000); // 3 sec intro
+    startMatrixIntro(10000); // 10 sec intro
 }
